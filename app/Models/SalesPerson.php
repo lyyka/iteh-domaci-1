f9f4af56bfd8b6ecc93818074618143c171e49b6
Luka Robajac <luka.robajac@gmail.com>
2022-11-04 12:15:35 +0100

    Add Product and SalesPerson models and DDL

---
+++ app/Models/SalesPerson.php
@@ -0,0 +1,56 @@
+<?php
+
+class SalesPerson
+{
+    private ?int $id = null;
+    private string $name;
+    private string $email;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+}
--- database/ddl.sql
+++ database/ddl.sql
@@ -16,3 +16,31 @@ create table contacts
     constraint contacts_id_uindex
         unique (id)
 );
+
+create table products
+(
+    id           bigint auto_increment
+        primary key,
+    product_name varchar(255) not null,
+    constraint products_id_uindex
+        unique (id)
+);
+
+create table sales_people
+(
+    id    bigint auto_increment
+        primary key,
+    name  varchar(255) not null,
+    email varchar(255) not null,
+    constraint sales_people_email_uindex
+        unique (email),
+    constraint sales_people_id_uindex
+        unique (id)
+);
+
+insert into products(product_name) values('Paket Usluga 1');
+insert into products(product_name) values('Paket Usluga 2');
+insert into products(product_name) values('Paket Usluga 3');
+
+insert into sales_people(name, email) values ('Marko Markovic', 'marko@markovic.com');
+insert into sales_people(name, email) values ('Luka Lukic', 'luka@lukic.com');
